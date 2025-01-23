1930464b2c49fa886d2d8ddd9a60fefa5781b68b
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-01-23 02:29:28 +0100

    Added workflows and tests

---
+++ scripts/build_and_run_unit_tests.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+BUILD_AND_RUN_UNIT_TESTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+BUILD_AND_RUN_UNIT_TESTS_REPO_DIR="${BUILD_AND_RUN_UNIT_TESTS_DIR}/.."
+
+TEST_TARGET_NAME="RenderEngineTest"
+
+set -euo pipefail  # Exit on error, unset variables, and pipeline failures
+
+# Step 1: cd to the repo root
+cd "${BUILD_AND_RUN_UNIT_TESTS_REPO_DIR}" || exit 1
+
+# Step 2: configure cmake
+
+mkdir -p build && cd build || exit 1
+cmake -DCMAKE_BUILD_TYPE=Debug .. || exit 1
+
+# Step 3: build the project (additionally verify if everything builds correctly)
+cmake --build .
+
+# Step 5: find test binary
+TEST_BINARY=$(find . -name "${TEST_TARGET_NAME}" -type f)
+
+# Step 6: run tests
+"${TEST_BINARY}" || exit 1
